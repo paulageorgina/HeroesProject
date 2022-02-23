e79c85be71c245b3a322714234aff6a6ffba81a9
JulianaRaS <kramirezsacarelo@gmail.com>
2022-02-23 00:49:41 -0500

    Controlador DC

--- Heroes/src/widgetsController/InsertarServicioController.java
+++ Heroes/src/widgetsController/InsertarServicioController.java
@@ -21,6 +21,36 @@ import widgets.HeroesJavaFx;
 
 public class InsertarServicioController implements Initializable{
 
+    // DC
+    @FXML
+    private Button BtnCerrarA;
+
+    @FXML
+    private Button BtnCerrarBatman;
+
+    @FXML
+    private Button BtnCerrarBlack;
+
+    @FXML
+    private Button BtnCerrarBlue;
+
+    @FXML
+    private Button BtnCerrarFlash;
+
+    @FXML
+    private Button BtnCerrarGreen;
+
+    @FXML
+    private Button BtnCerrarMartian;
+
+    @FXML
+    private Button BtnCerrarRobin;
+    @FXML
+    private Button BtnCerrarSuperman;
+
+    @FXML
+    private Button BtnCerrarWonder;
+
   // MARVEL
 
   @FXML
@@ -129,6 +159,258 @@ public class InsertarServicioController implements Initializable{
           Logger.getLogger(HeroesJavaFx.class.getName()).log(Level.SEVERE, null, ex);
       }
   }
+
+  // DC
+  public void closeWindowsArrow() {
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../../Container/Usuario.fxml"));
+        Parent root = loader.load();
+        HeroesJavaFx controlador = loader.getController();
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        Image ICON = new Image("https://herosjs.herokuapp.com/herosjs.git/SpuerHeroes.png");
+
+        stage.getIcons().add(ICON);
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        stage.setScene(scene);
+        stage.show();
+        stage.setOnCloseRequest(e -> controlador.cargarArrow());
+
+        Stage myStage = (Stage) this.BtnCerrarA.getScene().getWindow();
+        myStage.close();
+
+    } catch (IOException ex) {
+        Logger.getLogger(HeroesJavaFx.class.getName()).log(Level.SEVERE, null, ex);
+    }
+}
+
+public void closeWindowsBatman() {
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../../Container/Usuario.fxml"));
+        Parent root = loader.load();
+        HeroesJavaFx controlador = loader.getController();
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+
+        Image ICON = new Image("https://herosjs.herokuapp.com/herosjs.git/SpuerHeroes.png");
+
+        stage.getIcons().add(ICON);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+        stage.show();
+        stage.setOnCloseRequest(e -> controlador.cargarBatman());
+
+        Stage myStage = (Stage) this.BtnCerrarBatman.getScene().getWindow();
+        myStage.close();
+
+    } catch (IOException ex) {
+        Logger.getLogger(HeroesJavaFx.class.getName()).log(Level.SEVERE, null, ex);
+    }
+}
+
+public void closeWindowsBlack() {
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../../Container/Usuario.fxml"));
+        Parent root = loader.load();
+        HeroesJavaFx controlador = loader.getController();
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+
+        Image ICON = new Image("https://herosjs.herokuapp.com/herosjs.git/SpuerHeroes.png");
+
+        stage.getIcons().add(ICON);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+        stage.show();
+
+        stage.setOnCloseRequest(e -> controlador.cargarBlack());
+
+        Stage myStage = (Stage) this.BtnCerrarBlack.getScene().getWindow();
+        myStage.close();
+
+    } catch (IOException ex) {
+        Logger.getLogger(HeroesJavaFx.class.getName()).log(Level.SEVERE, null, ex);
+    }
+}
+
+public void closeWindowsBlue() {
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../../Container/Usuario.fxml"));
+        Parent root = loader.load();
+        HeroesJavaFx controlador = loader.getController();
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+
+        Image ICON = new Image("https://herosjs.herokuapp.com/herosjs.git/SpuerHeroes.png");
+
+        stage.getIcons().add(ICON);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+        stage.show();
+
+        stage.setOnCloseRequest(e -> controlador.cargarBlue());
+        Stage myStage = (Stage) this.BtnCerrarBlue.getScene().getWindow();
+        myStage.close();
+
+    } catch (IOException ex) {
+        Logger.getLogger(HeroesJavaFx.class.getName()).log(Level.SEVERE, null, ex);
+    }
+}
+
+public void closeWindowsFlash() {
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../../Container/Usuario.fxml"));
+        Parent root = loader.load();
+        HeroesJavaFx controlador = loader.getController();
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+
+        Image ICON = new Image("https://herosjs.herokuapp.com/herosjs.git/SpuerHeroes.png");
+
+        stage.getIcons().add(ICON);
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        stage.setScene(scene);
+        stage.show();
+        stage.setOnCloseRequest(e -> controlador.cargarFalsh());
+
+        Stage myStage = (Stage) this.BtnCerrarFlash.getScene().getWindow();
+        myStage.close();
+
+    } catch (IOException ex) {
+        Logger.getLogger(HeroesJavaFx.class.getName()).log(Level.SEVERE, null, ex);
+    }
+}
+
+public void closeWindowsGreen() {
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../../Container/Usuario.fxml"));
+        Parent root = loader.load();
+        HeroesJavaFx controlador = loader.getController();
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+
+        Image ICON = new Image("https://herosjs.herokuapp.com/herosjs.git/SpuerHeroes.png");
+
+        stage.getIcons().add(ICON);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+        stage.show();
+        stage.setOnCloseRequest(e -> controlador.cargarGreenLantern());
+
+        Stage myStage = (Stage) this.BtnCerrarGreen.getScene().getWindow();
+        myStage.close();
+
+    } catch (IOException ex) {
+        Logger.getLogger(HeroesJavaFx.class.getName()).log(Level.SEVERE, null, ex);
+    }
+}
+
+public void closeWindowsMartian() {
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../../Container/Usuario.fxml"));
+        Parent root = loader.load();
+        HeroesJavaFx controlador = loader.getController();
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        Image ICON = new Image("https://herosjs.herokuapp.com/herosjs.git/SpuerHeroes.png");
+
+        stage.getIcons().add(ICON);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+        stage.show();
+        stage.setOnCloseRequest(e -> controlador.cargarMartial());
+
+        Stage myStage = (Stage) this.BtnCerrarMartian.getScene().getWindow();
+        myStage.close();
+
+    } catch (IOException ex) {
+        Logger.getLogger(HeroesJavaFx.class.getName()).log(Level.SEVERE, null, ex);
+    }
+}
+
+public void closeWindowsRobin() {
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../../Container/Usuario.fxml"));
+        Parent root = loader.load();
+        HeroesJavaFx controlador = loader.getController();
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+
+        Image ICON = new Image("https://herosjs.herokuapp.com/herosjs.git/SpuerHeroes.png");
+
+        stage.getIcons().add(ICON);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+        stage.show();
+
+        stage.setOnCloseRequest(e -> controlador.cargarRobin());
+
+        Stage myStage = (Stage) this.BtnCerrarRobin.getScene().getWindow();
+        myStage.close();
+
+    } catch (IOException ex) {
+        Logger.getLogger(HeroesJavaFx.class.getName()).log(Level.SEVERE, null, ex);
+    }
+}
+
+public void closeWindowsSuperman() {
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../../Container/Usuario.fxml"));
+        Parent root = loader.load();
+        HeroesJavaFx controlador = loader.getController();
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+
+        Image ICON = new Image("https://herosjs.herokuapp.com/herosjs.git/SpuerHeroes.png");
+
+        stage.getIcons().add(ICON);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+        stage.show();
+        stage.setOnCloseRequest(e -> controlador.cargarSuperman());
+
+        Stage myStage = (Stage) this.BtnCerrarSuperman.getScene().getWindow();
+        myStage.close();
+
+    } catch (IOException ex) {
+        Logger.getLogger(HeroesJavaFx.class.getName()).log(Level.SEVERE, null, ex);
+    }
+}
+
+public void closeWindowsWonder() {
+    try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../../Container/Usuario.fxml"));
+        Parent root = loader.load(); 
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+
+        Image ICON = new Image("https://herosjs.herokuapp.com/herosjs.git/SpuerHeroes.png");
+
+        stage.getIcons().add(ICON);
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        stage.setScene(scene);
+        stage.show();
+
+        Stage myStage = (Stage) this.BtnCerrarWonder.getScene().getWindow();
+        myStage.close();
+
+    } catch (IOException ex) {
+        Logger.getLogger(HeroesJavaFx.class.getName()).log(Level.SEVERE, null, ex);
+    }
+}
 
   // MARVEL
 
